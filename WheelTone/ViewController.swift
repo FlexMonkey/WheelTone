67567f128834a87e743d54bd3263a4887674e549
Simon Gladman <sgladman@gmail.com>
2015-04-25 10:31:10 +0100

    Initial Commit

---
+++ WheelTone/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  WheelTone
+//
+//  Created by Simon Gladman on 25/04/2015.
+//  Copyright (c) 2015 Simon Gladman. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
